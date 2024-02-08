c3ce2c1fb063a21e38b5930f2acf0c8c36c6f45b
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-02-08 00:31:26 -0300

    feat: :sparkles: create migrations and post method for user registration

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def home(request): 
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    pass
-    
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+
+        # Verificar se o campo "nome" está preenchido
+        if nome:
+            novo_usuario = Usuario(nome=nome, idade=idade)
+            novo_usuario.save()
+        else:
+            # Se o campo "nome" não estiver preenchido, você pode fornecer um feedback ao usuário
+            # e talvez redirecioná-lo de volta ao formulário
+            return render(request, 'usuarios/usuarios.html', {'error_message': 'O campo nome é obrigatório'})
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    return render(request, 'usuarios/usuarios.html', usuarios)
